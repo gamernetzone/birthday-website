8457ce90aab63a8bb7e3e4df3b939cb3186347b3
gamernetzone <gamernetzone@gmail.com>
2026-07-06 13:54:26 +0530

    Fix audio playback

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,19 +1,20 @@
-import { FaMusic } from "react-icons/fa";
 function MusicPlayer() {
-return (
-  <div className="music-player">
+  const handlePlay = () => {
+    document.getElementById('audio').play();
+  };
 
-    <h3>
-      <FaMusic /> Birthday Music
-    </h3>
-
-    <audio autoPlay loop controls>
-      <source src="/birthday.mp3" type="audio/mpeg" />
-      Your browser does not support audio.
-    </audio>
-
-  </div>
-);
+  return (
+    <div className="music-player">
+      <h3><FaMusic /> Birthday Music</h3>
+      <button onClick={handlePlay}>
+        Play Music
+      </button>
+      <audio id="audio" loop>
+        <source src="/birthday.mp3" type="audio/mpeg" />
+        Your browser does not support audio.
+      </audio>
+    </div>
+  );
 }
 
 export default MusicPlayer;
